97809054b6937abd3e2026b4a06bf2627728f1d7
mahmoud ali <mahmoudalio353@gmail.com>
2023-07-07 15:23:16 +0300

    create appbar

--- lib/views/note_view.dart
+++ lib/views/note_view.dart
@@ -5,6 +5,10 @@ class Note_views extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Notes"),
+      ),
+    );
   }
 }
