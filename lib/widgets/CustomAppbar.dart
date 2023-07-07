ed5a7247abe6dbaf90c1109f66b4f38e6ef28749
mahmoud ali <mahmoudalio353@gmail.com>
2023-07-07 15:36:21 +0300

    create CustomAppbar

---
+++ lib/widgets/CustomAppbar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomerAppBar extends StatelessWidget {
+  const CustomerAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row();
+  }
+}
--- lib/widgets/note_view_body.dart
+++ lib/widgets/note_view_body.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:last_pro/widgets/CustomAppbar.dart';
 
 class note_view_body extends StatelessWidget {
   const note_view_body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        CustomerAppBar(),
+      ],
+    );
   }
 }
