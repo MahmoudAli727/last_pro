16a1946dfa67f4800242c7d053a9e4a9395059ce
mahmoud ali <mahmoudalio353@gmail.com>
2023-07-08 22:55:56 +0300

    Create Edit_view

---
+++ lib/views/Edit_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Edit_view extends StatelessWidget {
+  const Edit_view({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [],
+    );
+  }
+}
--- lib/widgets/Note_View_itme.dart
+++ lib/widgets/Note_View_itme.dart
@@ -1,56 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:last_pro/views/Edit_view.dart';
 
 class Note_View_itme extends StatelessWidget {
   const Note_View_itme({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(top: 18, bottom: 18, left: 18),
-      decoration: BoxDecoration(
-        color: Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text(
-              "Flutter Tips",
-              style: TextStyle(
-                fontSize: 22,
-                color: Colors.black,
-              ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 14, bottom: 12),
-              child: Text(
-                "Bulid your career with Mahmoud Ali",
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(
+          builder: (context) {
+            return Edit_view();
+          },
+        ));
+      },
+      child: Container(
+        padding: EdgeInsets.only(top: 18, bottom: 18, left: 18),
+        decoration: BoxDecoration(
+          color: Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text(
+                "Flutter Tips",
                 style: TextStyle(
-                  fontSize: 14,
+                  fontSize: 22,
+                  color: Colors.black,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 14, bottom: 12),
+                child: Text(
+                  "Bulid your career with Mahmoud Ali",
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    FontAwesomeIcons.trash,
+                    color: Colors.black,
+                    size: 24,
+                  )),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 16),
+              child: Text(
+                "May21 , 2022",
+                style: TextStyle(
                   color: Colors.black.withOpacity(0.5),
                 ),
               ),
             ),
-            trailing: IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  FontAwesomeIcons.trash,
-                  color: Colors.black,
-                  size: 24,
-                )),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 16),
-            child: Text(
-              "May21 , 2022",
-              style: TextStyle(
-                color: Colors.black.withOpacity(0.5),
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
