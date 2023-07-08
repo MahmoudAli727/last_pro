59bb9780bb6485d8df17f507385bd08f59a335d6
mahmoud ali <mahmoudalio353@gmail.com>
2023-07-08 08:57:39 +0300

    create icon for item

--- lib/widgets/note_view_body.dart
+++ lib/widgets/note_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:last_pro/widgets/CustomAppbar.dart';
 
 class note_view_body extends StatelessWidget {
@@ -14,6 +15,46 @@ class note_view_body extends StatelessWidget {
             height: 25,
           ),
           CustomerAppBar(),
+          Note_View_itme(),
+        ],
+      ),
+    );
+  }
+}
+
+class Note_View_itme extends StatelessWidget {
+  const Note_View_itme({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(0xffFFCC80),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text(
+              "Flutter Tips",
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+            subtitle: Text(
+              "Bulid your career with Mahmoud Ali",
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+            trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 28,
+                )),
+          ),
         ],
       ),
     );
