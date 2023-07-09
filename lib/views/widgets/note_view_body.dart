ed51aeef4b155cd4c351c4486bef24f1796557d1
mahmoud ali <mahmoudalio353@gmail.com>
2023-07-09 09:50:45 +0300

    add foder wid to view fol

--- lib/views/widgets/note_view_body.dart
+++ lib/views/widgets/note_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:last_pro/views/widgets/notelist.dart';
 import 'package:last_pro/widgets/CustomAppbar.dart';
-import 'package:last_pro/widgets/notelist.dart';
 
 class note_view_body extends StatelessWidget {
   const note_view_body({super.key});
